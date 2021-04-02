c199c5c6d927ee9063d4bdafeda673da4b2352c1
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-04-02 18:10:01 +0900

    Replace MainActivity

--- android/app/src/main/kotlin/com/example/flutter_sample_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_sample_app/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.example.flutter_sample_app
 
-import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_layout.*
 
-class MainActivity: FlutterActivity() {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        open_flutter_activity_button.setOnClickListener {
+            startActivity(Intent(this, SampleFlutterActivity::class.java))
+        }
+
+        open_flutter_fragment_button.setOnClickListener {
+
+        }
+    }
 }
